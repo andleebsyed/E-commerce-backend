046f3b8ae0552f1e31b228a614a463134eba9410
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-14 02:00:44 +0530

    refactor: refactored routes as they were cluttered

--- routers/wishlist-router.js
+++ routers/wishlist-router.js
@@ -1,39 +1,51 @@
-const express = require('express')
+const express = require("express");
+const { verifyToken } = require("../middlewares/verifyToken");
+const { User } = require("../models/user-model");
 const WishlistRouter = express.Router();
-const {Wishlist} = require('../models/wishlist-model')
+const { Wishlist } = require("../models/wishlist-model");
 
-WishlistRouter.route('/')
-.get(async (req,  res) =>{
+WishlistRouter.get("/", async (req, res) => {
   // get all
-  try{
-    const myWishlist = await Wishlist.find({})
-    res.json({myWishlist : myWishlist })
+  try {
+    const myWishlist = await Wishlist.find({});
+    res.json({ myWishlist: myWishlist });
+  } catch (error) {
+    res.json({ status: false, message: "failed", errMessage: error.message });
   }
-  catch(error){
-    res.json({status : false , message : "failed" , errMessage : error.message})
+});
+WishlistRouter.post("/", verifyToken, async (req, res) => {
+  try {
+    const { userId, productId } = req.body;
+    const user = await User.findById(userId);
+    user.wishlist.push(productId);
+    const response = await user.save();
+
+    res.json({
+      status: true,
+      response,
+      message: "added to wishlist successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: "sorry couldn't add the product",
+      errMessage: error.message,
+    });
   }
+});
 
-})
-.post(async (req  , res) => {
-  try{
-    const data = req.body
-    const NewProduct = new Wishlist(data)
-    const SavedProduct =await  NewProduct.save()
-    res.json({SavedProduct : SavedProduct})
-  }catch(error){ res.status(500).json({status : false  ,
-  message : "sorry couldn't add the product" , errMessage : error.message })}
-})
-
-WishlistRouter.route('/:id')
-.delete(async (req, res) =>{
-  const {id} = req.params
-  try{
-      await Wishlist.remove({_id : id})
-      res.json({staus : true , message : 'deleted successfully'})
-  }catch(error) {
-    res.json({status : false , message : "something wrong happened" , errorMessaage : error.message})
+WishlistRouter.route("/:id").delete(async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Wishlist.remove({ _id: id });
+    res.json({ staus: true, message: "deleted successfully" });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: "something wrong happened",
+      errorMessaage: error.message,
+    });
   }
-})
+});
 
-module.exports = {WishlistRouter};
-
+module.exports = { WishlistRouter };
