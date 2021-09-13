4554e3145bec2eac12654a932ad05ce72bf67852
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-13 15:56:52 +0530

    update: added token verifier middleware and used it in Account route

--- routers/user-router.js
+++ routers/user-router.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { SignUp, Login } = require("../controllers/users");
+const { SignUp, Login, Account } = require("../controllers/users");
+const { verifyToken } = require("../middlewares/verifyToken");
 const UserRouter = express.Router();
 UserRouter.post("/signup", SignUp);
 UserRouter.post("/login", Login);
+UserRouter.post("/account", verifyToken, Account);
 
 module.exports = { UserRouter };
