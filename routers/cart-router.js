5414c4e2c85a6ead2223bb4f322cd3d2e5677820
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-14 02:43:17 +0530

    update: added controllefr for adding a product to the cart

--- routers/cart-router.js
+++ routers/cart-router.js
@@ -1,39 +1,54 @@
-const express = require('express')
-const { Cart } = require('../models/cart-model')
-const CartRouter = express.Router()
-CartRouter.route('/')
+const express = require("express");
+const { verifyToken } = require("../middlewares/verifyToken");
+const { Cart } = require("../models/cart-model");
+const { User } = require("../models/user-model");
+const CartRouter = express.Router();
+CartRouter.route("/")
   .get(async (req, res) => {
     try {
-      const myCart = await Cart.find({})
-      res.json({ myCart: myCart })
+      const myCart = await Cart.find({});
+      res.json({ myCart: myCart });
     } catch (error) {
-      res.status(400).json({ status: false, message: "couldn't collect the data", errorMessaage: error.message })
+      res.status(400).json({
+        status: false,
+        message: "couldn't collect the data",
+        errorMessaage: error.message,
+      });
     }
-
   })
-  .post(async (req, res) => {
+  .post(verifyToken, async (req, res) => {
     try {
-      const data = req.body
-      const newProduct = new Cart(data)
-      const SavedData = await newProduct.save()
-      res.json({ SavedData: SavedData })
-    }
-    catch (error) {
-      res.status(400).json({ status: false, message: "Failed to update the cart'", errorMessaage: error.message })
+      const { productId, userId } = req.body;
+      const user = await User.findById(userId);
+      user.cart.push(productId);
+      // const newProduct = new Cart(data)
+      // const SavedData = await newProduct.save()
+      const response = await user.save();
+      res.json({
+        status: true,
+        message: "profduct saved to cart successfully",
+        response,
+      });
+    } catch (error) {
+      res.json({
+        status: false,
+        message: "Failed to update the cart'",
+        errorMessaage: error.message,
+      });
     }
   })
 
   .put(async (req, res) => {
-    const id = req.body._id
-    const paramCase = req.param('case')
+    const id = req.body._id;
+    const paramCase = req.param("case");
     // for increment
-    if (paramCase === 'inc') {
+    if (paramCase === "inc") {
       try {
         Cart.updateOne(
           { _id: id },
           {
-            $inc: { quantity: 1 }
-          },
+            $inc: { quantity: 1 },
+          }
 
           // (err, result) => {
           //   if (err) {
@@ -42,10 +57,9 @@ CartRouter.route('/')
           //     res.json(result);
           //   }
           // }
-        )
-      }
-      catch (error) {
-        res.json({ message: "failed", errMessage: error.message })
+        );
+      } catch (error) {
+        res.json({ message: "failed", errMessage: error.message });
       }
     }
 
@@ -55,8 +69,8 @@ CartRouter.route('/')
         Cart.updateOne(
           { _id: id },
           {
-            $inc: { quantity: -1 }
-          },
+            $inc: { quantity: -1 },
+          }
 
           // (err, result) => {
           //   if (err) {
@@ -65,24 +79,25 @@ CartRouter.route('/')
           //     res.json(result);
           //   }
           // }
-        )
-      }
-      catch (error) {
-        res.json({ message: "failed", errMessage: error.message })
+        );
+      } catch (error) {
+        res.json({ message: "failed", errMessage: error.message });
       }
     }
+  });
 
-  })
+CartRouter.route("/:id").delete(async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Cart.remove({ _id: id });
+    res.json({ staus: true, message: "deleted successfully" });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: "something wrong happened",
+      errorMessaage: error.message,
+    });
+  }
+});
 
-CartRouter.route('/:id')
-  .delete(async (req, res) => {
-    const { id } = req.params
-    try {
-      await Cart.remove({ _id: id })
-      res.json({ staus: true, message: 'deleted successfully' })
-    } catch (error) {
-      res.json({ status: false, message: "something wrong happened", errorMessaage: error.message })
-    }
-  })
-
-module.exports = { CartRouter }
+module.exports = { CartRouter };
